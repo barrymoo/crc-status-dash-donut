8eb5a68b985dd38dc79e33071e21976cfcbce3b5
Barry Moore <moore0557@gmail.com>
2019-02-07 14:24:41 -0500

    Apply black formatting

--- app.py
+++ app.py
@@ -9,119 +9,110 @@ import os
 
 def cluster_plot_layout(name):
     return go.Layout(
-        showlegend = False,
-        margin = {
-            'l': 1,
-            'r': 1,
-            't': 1,
-            'b': 1,
-        },
-        annotations = [
+        showlegend=False,
+        margin={"l": 1, "r": 1, "t": 1, "b": 1},
+        annotations=[
             {
-                'visible': True,
-                'text': name,
-                'x': 0.5,
-                'y': 0.5,
-                'showarrow': False,
-                'font': {
-                    'size': 30,
-                },
-            },
+                "visible": True,
+                "text": name,
+                "x": 0.5,
+                "y": 0.5,
+                "showarrow": False,
+                "font": {"size": 30},
+            }
         ],
     )
 
 
-def cluster_plot_traces(labels, vals):    
-    return [go.Pie(
-            labels = labels,
-            values = vals,
-            marker = {'colors':
-                [
-                    "rgb(181, 160, 97)",
-                    "rgb(13, 75, 116)",
-                ]
-            },
-            textinfo = "label",
-            textposition = "inside",
-            textfont = {
-                'size': 24,
-                'color': '#ffffff',
-            },
-            hole = 0.65,
-            hoverinfo = 'value+text+percent',
-            sort = False,
+def cluster_plot_traces(labels, vals):
+    return [
+        go.Pie(
+            labels=labels,
+            values=vals,
+            marker={"colors": ["rgb(181, 160, 97)", "rgb(13, 75, 116)"]},
+            textinfo="label",
+            textposition="inside",
+            textfont={"size": 24, "color": "#ffffff"},
+            hole=0.65,
+            hoverinfo="value+text+percent",
+            sort=False,
         )
     ]
 
 
 def get_single_entry(cluster):
-    cursor = db['status'].find({'cluster': cluster}).sort('_id', pymongo.DESCENDING).limit(1)
+    cursor = (
+        db["status"].find({"cluster": cluster}).sort("_id", pymongo.DESCENDING).limit(1)
+    )
     for item in list(cursor):
-        return [item['allocated'], item['total'] - item['allocated']]
+        return [item["allocated"], item["total"] - item["allocated"]]
 
 
 def generate_smp_figure(labels):
     return {
-        'data': cluster_plot_traces(labels, get_single_entry("smp")),
-        'layout': cluster_plot_layout("SMP")
+        "data": cluster_plot_traces(labels, get_single_entry("smp")),
+        "layout": cluster_plot_layout("SMP"),
     }
 
 
 def generate_gpu_figure(labels):
     return {
-        'data': cluster_plot_traces(labels, get_single_entry("gpu")),
-        'layout': cluster_plot_layout("GPU")
+        "data": cluster_plot_traces(labels, get_single_entry("gpu")),
+        "layout": cluster_plot_layout("GPU"),
     }
 
 
 def generate_mpi_figure(labels):
     return {
-        'data': cluster_plot_traces(labels, get_single_entry("mpi")),
-        'layout': cluster_plot_layout("MPI")
+        "data": cluster_plot_traces(labels, get_single_entry("mpi")),
+        "layout": cluster_plot_layout("MPI"),
     }
 
 
 def generate_htc_figure(labels):
     return {
-        'data': cluster_plot_traces(labels, get_single_entry("htc")),
-        'layout': cluster_plot_layout("HTC")
+        "data": cluster_plot_traces(labels, get_single_entry("htc")),
+        "layout": cluster_plot_layout("HTC"),
     }
 
 
 # The layout function, this allows for the page updates when navigating to the site
 def generate_layout(labels):
-    return html.Div([
-            html.Div([
+    return html.Div(
+        [
+            html.Div(
+                [
                     dcc.Graph(
-                        id = 'smp-graph',
-                        figure = generate_smp_figure(labels),
-                        style = {'display': 'flex', 'width': '130px'}
+                        id="smp-graph",
+                        figure=generate_smp_figure(labels),
+                        style={"display": "flex", "width": "130px"},
                     ),
                     dcc.Graph(
-                        id = 'gpu-graph',
-                        figure = generate_gpu_figure(labels),
-                        style = {'display': 'flex', 'width': '130px'}
-                    )
-                    ], style = {'display': 'flex', 'width': '260px', 'height': '122px'}
+                        id="gpu-graph",
+                        figure=generate_gpu_figure(labels),
+                        style={"display": "flex", "width": "130px"},
+                    ),
+                ],
+                style={"display": "flex", "width": "260px", "height": "122px"},
             ),
-            html.Div([
+            html.Div(
+                [
                     dcc.Graph(
-                        id = 'mpi-graph',
-                        figure = generate_mpi_figure(labels),
-                        style = {'display': 'flex', 'width': '130px'}
+                        id="mpi-graph",
+                        figure=generate_mpi_figure(labels),
+                        style={"display": "flex", "width": "130px"},
                     ),
                     dcc.Graph(
-                        id = 'htc-graph',
-                        figure = generate_htc_figure(labels),
-                        style = {'display': 'flex', 'width': '130px'}
+                        id="htc-graph",
+                        figure=generate_htc_figure(labels),
+                        style={"display": "flex", "width": "130px"},
                     ),
-                ], style = {'display': 'flex', 'width': '260px', 'height': '122px'}
+                ],
+                style={"display": "flex", "width": "260px", "height": "122px"},
             ),
-            dcc.Interval(
-                id = 'interval-component',
-                interval = 5 * 60 * 1000
-            )
-        ], style = {'width': '260px', 'height': '245px'}
+            dcc.Interval(id="interval-component", interval=5 * 60 * 1000),
+        ],
+        style={"width": "260px", "height": "245px"},
     )
 
 
@@ -129,10 +120,10 @@ def generate_layout(labels):
 app = dash.Dash(__name__)
 server = app.server
 # -> This part is important for Heroku deployment
-server.secret_key = os.environ['SECRET_KEY']
+server.secret_key = os.environ["SECRET_KEY"]
 
 # Ready the database
-uri = os.environ['MONGO_URI']
+uri = os.environ["MONGO_URI"]
 client = pymongo.MongoClient(uri)
 db = client.get_default_database()
 
@@ -140,35 +131,39 @@ db = client.get_default_database()
 labels = ["Used", "Free"]
 
 # The app layout w/ wrapper for layout
-#wrapper = generate_layout(labels)
-#app.layout = wrapper
+# wrapper = generate_layout(labels)
+# app.layout = wrapper
 app.layout = lambda: generate_layout(labels)
 
 
-@app.callback(Output('smp-graph', 'figure'),
-              events=[Event('interval-component', 'interval')])
+@app.callback(
+    Output("smp-graph", "figure"), events=[Event("interval-component", "interval")]
+)
 def update_smp_graph():
     return generate_smp_figure(labels)
 
 
-@app.callback(Output('gpu-graph', 'figure'),
-              events=[Event('interval-component', 'interval')])
+@app.callback(
+    Output("gpu-graph", "figure"), events=[Event("interval-component", "interval")]
+)
 def update_gpu_graph():
     return generate_gpu_figure(labels)
 
 
-@app.callback(Output('mpi-graph', 'figure'),
-              events=[Event('interval-component', 'interval')])
+@app.callback(
+    Output("mpi-graph", "figure"), events=[Event("interval-component", "interval")]
+)
 def update_mpi_graph():
     return generate_mpi_figure(labels)
 
 
-@app.callback(Output('htc-graph', 'figure'),
-              events=[Event('interval-component', 'interval')])
+@app.callback(
+    Output("htc-graph", "figure"), events=[Event("interval-component", "interval")]
+)
 def update_htc_graph():
     return generate_htc_figure(labels)
 
 
 # Our main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server()
