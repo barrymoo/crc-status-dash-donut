1556049c8c826167f89191c70aaf4adf8f22e120
Barry Moore <moore0557@gmail.com>
2019-04-02 14:42:04 -0400

    Switch to uwsgi, create callback for data necessary in new dash version

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,11 @@ WORKDIR /app
 COPY app.py requirements.txt /app/
 RUN apt update
 RUN apt upgrade -y --no-install-recommends
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y \
-    python3 python3-pip python3-setuptools python3-wheel \
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y python3 python3-dev \
+    python3-pip python3-setuptools python3-wheel build-essential \
     --no-install-recommends
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/*
 RUN pip3 install -r requirements.txt
-CMD ["gunicorn", "-w", "1", "-b", ":8000", "app:server"]
+EXPOSE 5000
+CMD ["uwsgi", "--http", ":5000", "--module", "app:server", "--uid", "nobody", "--master"]
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dash.dependencies import Output, Input
 import plotly.graph_objs as go
 import pymongo
 import os
+import json
 
 
 def cluster_plot_layout(name):
@@ -40,60 +41,52 @@ def cluster_plot_traces(labels, vals):
     ]
 
 
-def query_most_recent_data():
-    items = list(
-        db["status"].find({}).sort("_id", pymongo.DESCENDING).limit(1)
-    )
-    data = {}
-    for item in items:
-        for cluster in clusters:
-            alloc = item[cluster]["alloc"]
-            data[cluster] = [alloc, item[cluster]["total"] - alloc]
-    return data
-
-
-def generate_smp_figure(labels):
+def generate_smp_figure(labels, data):
+    d = json.loads(data)["smp"]
     return {
-        "data": cluster_plot_traces(labels, to_display["smp"]),
+        "data": cluster_plot_traces(labels, d),
         "layout": cluster_plot_layout("SMP"),
     }
 
 
-def generate_gpu_figure(labels):
+def generate_gpu_figure(labels, data):
+    d = json.loads(data)["gpu"]
     return {
-        "data": cluster_plot_traces(labels, to_display["gpu"]),
+        "data": cluster_plot_traces(labels, d),
         "layout": cluster_plot_layout("GPU"),
     }
 
 
-def generate_mpi_figure(labels):
+def generate_mpi_figure(labels, data):
+    d = json.loads(data)["mpi"]
     return {
-        "data": cluster_plot_traces(labels, to_display["mpi"]),
+        "data": cluster_plot_traces(labels, d),
         "layout": cluster_plot_layout("MPI"),
     }
 
 
-def generate_htc_figure(labels):
+def generate_htc_figure(labels, data):
+    d = json.loads(data)["htc"]
     return {
-        "data": cluster_plot_traces(labels, to_display["htc"]),
+        "data": cluster_plot_traces(labels, d),
         "layout": cluster_plot_layout("HTC"),
     }
 
 
 # The layout function, this allows for the page updates when navigating to the site
-def generate_layout(labels):
+def generate_layout(labels, data):
     return html.Div(
         [
             html.Div(
                 [
                     dcc.Graph(
                         id="smp-graph",
-                        figure=generate_smp_figure(labels),
+                        figure=generate_smp_figure(labels, data),
                         style={"display": "flex", "width": "130px"},
                     ),
                     dcc.Graph(
                         id="gpu-graph",
-                        figure=generate_gpu_figure(labels),
+                        figure=generate_gpu_figure(labels, data),
                         style={"display": "flex", "width": "130px"},
                     ),
                 ],
@@ -103,29 +96,41 @@ def generate_layout(labels):
                 [
                     dcc.Graph(
                         id="mpi-graph",
-                        figure=generate_mpi_figure(labels),
+                        figure=generate_mpi_figure(labels, data),
                         style={"display": "flex", "width": "130px"},
                     ),
                     dcc.Graph(
                         id="htc-graph",
-                        figure=generate_htc_figure(labels),
+                        figure=generate_htc_figure(labels, data),
                         style={"display": "flex", "width": "130px"},
                     ),
                 ],
                 style={"display": "flex", "width": "260px", "height": "122px"},
             ),
+            html.Div(id="data", style={"display": "none"}),
             dcc.Interval(
-                id="interval-component",
-                interval=300000, # 5 minutes
-                n_intervals=0
+                id="interval-component", interval=300000, n_intervals=0  # 5 minutes
             ),
         ],
         style={"width": "260px", "height": "245px"},
     )
 
 
+def query_most_recent_data():
+    items = list(db["status"].find({}).sort("_id", pymongo.DESCENDING).limit(1))
+    data = {}
+    for item in items:
+        for cluster in clusters:
+            alloc = item[cluster]["alloc"]
+            data[cluster] = [alloc, item[cluster]["total"] - alloc]
+    return json.dumps(data)
+
+
 # Initialize the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=["https://codepen.io/barrymoo/pen/rbaKVJ.css"],
+)
 server = app.server
 
 # Ready the database
@@ -133,47 +138,68 @@ uri = os.environ["MONGO_URI"]
 client = pymongo.MongoClient(uri)
 db = client.get_database()
 
+# Global variables which do not change
 clusters = ["smp", "gpu", "mpi", "htc"]
-
-to_display = query_most_recent_data()
-
-# Useful variables
 labels = ["Used", "Free"]
 
 # The app layout w/ wrapper for layout
 # wrapper = generate_layout(labels)
 # app.layout = wrapper
-app.layout = lambda: generate_layout(labels)
+initial_data = query_most_recent_data()
+app.layout = lambda: generate_layout(labels, initial_data)
 
 
 @app.callback(
-    Output("smp-graph", "figure"), [Input("interval-component", "n_intervals")]
+    Output("data", "children"),
+    [Input("interval-component", "n_intervals")],
 )
-def update_smp_graph(_):
-    return generate_smp_figure(labels)
+def query_most_recent_data_callback(_):
+    return query_most_recent_data()
 
 
 @app.callback(
-    Output("gpu-graph", "figure"), [Input("interval-component", "n_intervals")]
+    Output("smp-graph", "figure"),
+    [
+        Input("interval-component", "n_intervals"),
+        Input("data", "children"),
+    ],
 )
-def update_gpu_graph(_):
-    return generate_gpu_figure(labels)
+def update_smp_graph(_, data):
+    return generate_smp_figure(labels, data)
 
 
 @app.callback(
-    Output("mpi-graph", "figure"), [Input("interval-component", "n_intervals")]
+    Output("gpu-graph", "figure"),
+    [
+        Input("interval-component", "n_intervals"),
+        Input("data", "children"),
+    ],
 )
-def update_mpi_graph(_):
-    return generate_mpi_figure(labels)
+def update_gpu_graph(_, data):
+    return generate_gpu_figure(labels, data)
 
 
 @app.callback(
-    Output("htc-graph", "figure"), [Input("interval-component", "n_intervals")]
+    Output("mpi-graph", "figure"),
+    [
+        Input("interval-component", "n_intervals"),
+        Input("data", "children"),
+    ]
 )
-def update_htc_graph(_):
-    return generate_htc_figure(labels)
+def update_mpi_graph(_, data):
+    return generate_mpi_figure(labels, data)
 
 
-# Our main function
+@app.callback(
+    Output("htc-graph", "figure"),
+    [
+        Input("interval-component", "n_intervals"),
+        Input("data", "children"),
+    ],
+)
+def update_htc_graph(_, data):
+    return generate_htc_figure(labels, data)
+
+
 if __name__ == "__main__":
     app.run_server()
